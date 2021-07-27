0065a54277bbf6b2021b661c32e39caf9b1cd7d0
Renato Douglas Zembrani <renato.zembrani@hotmail.com>
2021-07-26 22:37:03 -0300

    Ĩncluding action on digit buttons to show on display

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,5 +2,19 @@ import React from 'react'
 
 import './Button.css'
 
-const button = props => <button className='button'>{props.label}</button>
+const button = props => {
+	let classStyle = 'button '
+	classStyle += props.operation ? 'operation' : ''
+	classStyle += props.double ? 'double' : ''
+	classStyle += props.triple ? 'triple' : ''
+	
+	return (
+		<button
+			className={classStyle}
+			onClick={e => props.click && props.click(e.target.innerHTML)}
+		>
+			{props.label}
+		</button>
+	)
+}
 export default button
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,28 +4,80 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {...initialState};
+    }
+    
+    clearMemory = () => {
+        this.setState({...initialState})
+    }
+
+    doOperation = (operation) => {
+        console.log(operation)
+    }
+
+    equal = () => {
+        console.log('equal')
+    }
+
+    sendDigits = (digit) => {
+        if (digit === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + digit
+        this.setState( { displayValue, clearDisplay: false })
+
+        if (digit !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+        }
+    }
+
     render () {
+        const {
+            clearMemory,
+            doOperation,
+            sendDigits,
+            equal
+        } = this;
+
         return (
             <div className='calculator'>
-                <Display value='100'/>
-                <Button label="AC"/>
-                <Button label="/"/>
-                <Button label="7"/>
-                <Button label="8"/>
-                <Button label="9"/>
-                <Button label="*"/>
-                <Button label="4"/>
-                <Button label="5"/>
-                <Button label="6"/>
-                <Button label="-"/>
-                <Button label="1"/>
-                <Button label="2"/>
-                <Button label="3"/>
-                <Button label="+"/>
-                <Button label="0"/>
-                <Button label="."/>
-                <Button label="="/>
+                <Display value={this.state.displayValue}/>
+                <Button label="AC" triple click={clearMemory}/>
+                <Button label="/" operation click={doOperation}/>
+                <Button label="7" click={sendDigits}/>
+                <Button label="8" click={sendDigits}/>
+                <Button label="9" click={sendDigits}/>
+                <Button label="*" operation click={doOperation}/>
+                <Button label="4" click={sendDigits}/>
+                <Button label="5" click={sendDigits}/>
+                <Button label="6" click={sendDigits}/>
+                <Button label="-" operation click={doOperation}/>
+                <Button label="1" click={sendDigits}/>
+                <Button label="2" click={sendDigits}/>
+                <Button label="3" click={sendDigits}/>
+                <Button label="+" operation click={doOperation}/>
+                <Button label="0" double click={sendDigits}/>
+                <Button label="." click={sendDigits}/>
+                <Button label="=" operation click={equal}/>
             </div>
         )
     }
